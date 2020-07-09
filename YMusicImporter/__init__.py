04b39a136a2443831a27370a95b04a519869d949
Palenov Yaroslav <palenov.yaroslav@gmail.com>
2020-07-09 16:16:25 +0300

    Create module

---
+++ YMusicImporter/__init__.py
@@ -0,0 +1,56 @@
+from yandex_music.client import Client
+from bs4 import BeautifulSoup
+import requests
+
+
+class YMusicImporter():
+    def __init__(self, email, password):
+        print(email, password)
+        self.client = Client.from_credentials(email, password)
+
+    def print_info(self, text):
+        print()
+        print("- - - - - - - - -")
+        print(text)
+        print("- - - - - - - - -")
+        print()
+
+    def import_playlist_from_apple(self, url):
+        self.print_info('TRY TO IMPORT PLAYLIST FROM {}'.format(url))
+        playlist = self.parse_apple_playlist(url)
+        self.create_playlist(playlist)
+        self.print_info('IMPORT COMPLETED')
+
+
+    def parse_apple_playlist(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, features="html.parser")
+        playlist = {
+            "title": "",
+            "tracks": []
+        }
+        playlist["title"] = " ".join(soup.find("h1", {"class": "product-name"}).contents).strip()
+        track_wrappers = soup.find_all("div", {"class": "song-name-wrapper"})
+        for track_wrapper in track_wrappers:
+            song_name = "".join(track_wrapper.find("div", {"class": "song-name"}).stripped_strings)
+            artist_name = track_wrapper.find("a").string
+            playlist["tracks"].append((song_name, artist_name))
+        return playlist
+
+
+    def create_playlist(self, playlist_info):
+        playlist = self.client.users_playlists_create(playlist_info["title"])
+        for i, track in enumerate(playlist_info["tracks"]):
+            track_number = i + 1
+            track_name = "{}".format(" - ".join(track))
+            res = self.client.search(text=track_name, type_="track")
+            if res and res.tracks:
+                playlist = self.client.users_playlists_insert_track(
+                    kind=playlist.kind,
+                    track_id=res.tracks.results[0].id,
+                    album_id=res.tracks.results[0].albums[0].id,
+                    revision=playlist.revision,
+                    at=playlist.track_count,
+                    timeout=60
+                )
+            print("{}. {}{}".format(track_number, track_name, "" if res and res.tracks else " [NOT FOUND] "))
